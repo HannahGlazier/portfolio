50a27a8edecd274e63d8983c77e4bdedaf3a7c65
Hannah Glazier <hrayglazier@aol.com>
2022-05-27 13:52:10 -0600

    Updated layout component and imported assets

--- client/src/components/Layout/ Layout.js
+++ client/src/components/Layout/ Layout.js
@@ -1,12 +1,22 @@
 import './index.scss';
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar/Sidebar'
 
 
 function  Layout() {
   return (
-    <div> Layout
-      <Sidebar />
+    <div className="App">
+        <Sidebar className="page"/>
+        <div className="page">
+          <span className="tags top-tags">&lt;body&gt;</span>
+          <Outlet />
+          <span className="tags bottom-tags">
+            &lt;/body&gt;
+            <br />
+            <span className="bottom-tag-html">&lt;/html&gt;</span>
+          </span>
+        </div>
     </div>
   )
 }
