168288d3882d1cf9bbcee21c10963394e2fc93d1
Hannah Glazier <hrayglazier@aol.com>
2022-05-27 09:42:28 -0600

    created component folder

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hannah Glazier's Portfolio
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
