1ca42a2986e75a26fbbbdc3e7a8043c8719235e2
Hannah Glazier <hrayglazier@aol.com>
2022-06-08 10:07:01 -0600

    Projects edits

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import './index.scss';
 
 function Projects() {
     return (
-        <div>Projects</div>
+        <div>
+            <h1>Diver</h1>
+            <h1>Citinerary</h1>
+            <h1>The Nighttime Routine</h1>
+            <h1>Recipe Box</h1>
+        </div>
     )
 }
 
