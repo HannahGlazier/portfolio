7ff90c1b320daf980e18f11ca37fd78084a1095c
Hannah Glazier <hrayglazier@aol.com>
2022-06-20 13:37:42 -0600

    initial project card template rendering

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -1,14 +1,38 @@
 import React from 'react'
-import './index.scss';
-// custom cards or material ui??
+import './Projects.scss';
+
+// Material UI Imports
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function Projects() {
     return (
         <div>
-            <h1>Diver</h1>
-            <h1>Citinerary</h1>
-            <h1>The Nighttime Routine</h1>
-            <h1>Recipe Box</h1>
+            <Card sx={{ maxWidth: 345 }} className="cards">
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image="https://images.unsplash.com/photo-1655721529468-d0d81b2dc489?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60"
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Share</Button>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+                </Card> 
         </div>
     )
 }
