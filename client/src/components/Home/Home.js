2fd48ac6a464e286032fb26c973269b63958dacb
Hannah Glazier <hrayglazier@aol.com>
2022-06-22 12:32:56 -0600

    main colors

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import './index.scss';
-// import HTest from '../../assets/images/HTest.png'
-// import h from '../../assets/images/h.png'
-// import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from "react-router-dom"
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
 import Loader from 'react-loaders'
@@ -34,7 +31,7 @@ function Home() {
                     <br /> 
                     <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={19}/>
                 </h1>
-                <h2> Fullstack Developer / Experienced in React - Rails Stack</h2>
+                <h2> Fullstack Developer / Experienced in the JavaScript - React.js - Rails</h2>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
             </div>
             <Loader type="pacman" />
